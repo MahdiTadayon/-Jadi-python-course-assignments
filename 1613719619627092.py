7d700c66d400cb037f9bdbdbbff88992bf766e17
MahdiTadayon <mahditadayon789@yahoo.com>
2023-01-08 14:27:07 +0330

    Update 1613719619627092.py

--- 1613719619627092.py
+++ 1613719619627092.py
@@ -1,7 +1,7 @@
 x=input().lower()
 y=''
 for i in range(0,len(x)):
-    if(x[i]!='a' and x[i]!='e' and  x[i]!='i' and x[i]!='o' and x[i]!='u'):
+    if not x[i] in 'aeiou' :
         y=y+'.'+x[i]
 
 print(y)
